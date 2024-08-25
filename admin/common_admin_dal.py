377fa80ad512fbb49c9bc53c4d702c56b32c0329
Sergey <s.kuzin@stik.pro>
2024-08-25 12:53:42 +0300

    admin delete tests, not_found handling for admin objects

--- admin/common_admin_dal.py
+++ admin/common_admin_dal.py
@@ -17,8 +17,7 @@ class CommonAdminDAL:
 
     async def get_full_objects_list(self):
         rows = await self.db_session.execute(select(self.model))
-        await self.db_session.commit()
-        return rows.unique().scalars().all()
+        return rows.unique().scalars().all() or []
 
     async def get_object_by_id(self, object_id: int):
         query = (
@@ -26,19 +25,18 @@ class CommonAdminDAL:
             .where(self.model.id == object_id)
         )
         result = await self.db_session.execute(query)
-        if not result:
+        obj = result.scalars().first()
+        if not obj:
             raise HTTPException(
                 status_code=HTTPStatus.NOT_FOUND,
                 detail="Объект не найден"
             )
-        await self.db_session.commit()
-        return result.scalars().first()
+        return obj
 
     async def create_object(self, data: dict):
         new_object = self.model(**data)
         self.db_session.add(new_object)
         await self.db_session.flush()
-        await self.db_session.commit()
         return new_object
 
     async def update_object(self, object_id: int, data: dict):
@@ -50,19 +48,24 @@ class CommonAdminDAL:
             .returning(self.model)
         )
         result = await self.db_session.execute(query)
-        if not result:
+        obj = result.scalars().first()
+        if not obj:
             raise HTTPException(
                 status_code=HTTPStatus.BAD_REQUEST,
                 detail="Объект не изменен"
             )
-        await self.db_session.commit()
-        return result.scalars().first()
+        return obj
 
     async def delete_object(self, object_id: int):
         query = (
             delete(self.model)
             .where(self.model.id == object_id)
+            .returning(self.model.id)
         )
-        await self.db_session.execute(query)
-        await self.db_session.commit()
+        result = await self.db_session.execute(query)
+        if not result.scalars().first():
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail="Объект не изменен"
+            )
         return Response(status_code=HTTPStatus.NO_CONTENT)
--- admin/common_admin_handlers.py
+++ admin/common_admin_handlers.py
@@ -1,4 +1,5 @@
 from http import HTTPStatus
+from fastapi import HTTPException
 
 from fastapi import Response
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -13,30 +14,35 @@ __all__ = (
 
 
 async def create_new_object(model, dal, response_model, body: dict, session: AsyncSession):
-    current_dal = dal(model, session)
-    created_object = await current_dal.create_object(body)
-    return response_model.model_validate(created_object, from_attributes=True)
+    async with session.begin():
+        current_dal = dal(model, session)
+        created_object = await current_dal.create_object(body)
+        return response_model.model_validate(created_object, from_attributes=True)
 
 
 async def get_object(model, dal, response_model, object_id, session: AsyncSession):
-    current_dal = dal(model, session)
-    created_object = await current_dal.get_object_by_id(object_id)
-    return response_model.model_validate(created_object, from_attributes=True)
+    async with session.begin():
+        current_dal = dal(model, session)
+        found_object = await current_dal.get_object_by_id(object_id)
+        return response_model.model_validate(found_object, from_attributes=True)
 
 
 async def update_object_by_id(model, dal, response_model, object_id: int, body: dict, session: AsyncSession):
-    current_dal = dal(model, session)
-    created_object = await current_dal.update_object(object_id, body)
-    return response_model.model_validate(created_object, from_attributes=True)
+    async with session.begin():
+        current_dal = dal(model, session)
+        updated_object = await current_dal.update_object(object_id, body)
+        return response_model.model_validate(updated_object, from_attributes=True)
 
 
 async def delete_object_by_id(model, dal, object_id, session: AsyncSession):
-    current_dal = dal(model, session)
-    await current_dal.delete_object(object_id)
-    return Response(status_code=HTTPStatus.NO_CONTENT)
+    async with session.begin():
+        current_dal = dal(model, session)
+        await current_dal.delete_object(object_id)
+        return Response(status_code=HTTPStatus.NO_CONTENT)
 
 
 async def get_objects_list(model, dal, response_model, session: AsyncSession):
-    current_dal = dal(model, session)
-    result = await current_dal.get_full_objects_list()
-    return [response_model.model_validate(row, from_attributes=True) for row in result]
+    async with session.begin():
+        current_dal = dal(model, session)
+        result = await current_dal.get_full_objects_list()
+        return [response_model.model_validate(row, from_attributes=True) for row in result]
